8e8a4c3a63cd0596f8e38031f33cd395420619c4
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-18 19:34:14 +0200

    parziale

--- js/script.js
+++ js/script.js
@@ -1,18 +1,47 @@
 $(document).ready(function(){
 
-// Mettiamo l'API in una variabile, settato su gennaio
-var endpoint = "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0";
 
+// Settiamo moment in italiano
+moment.locale('it');
+// Stabiliamo qual è la data iniziale
+var dataIniziale = moment('2018-04-01');
+var mese = dataIniziale.format("M");
+var anno = dataIniziale.format("YYYY");
+
+// Appendiamo mese e anno in pagina
+$("h1#mese").text(dataIniziale.format("MMMM"));
+$("h2#anno").text(anno);
+
+// Mettiamo l'API in una variabile, settato su gennaio
+var endpoint = "https://flynn.boolean.careers/exercises/api/holidays?year="+anno+"&month="+mese;
+
+// Prendiamo il template di handlebars
+var source = $("#day-template").html();
+var template = Handlebars.compile(source);
+
+// Scriviamo il giorno e il mese in base a dataIniziale
+for( var i = 0; i < dataIniziale.daysInMonth(); i++) {
+  var contenuto = {
+    "data-template": dataIniziale.format("YYYY-MM-DD"),
+    "day": dataIniziale.format("DD"),
+    "month": dataIniziale.format('MMMM')
+  }
+  $("#days").append(template(contenuto));
+  dataIniziale.add(1, "days");
+}
+
+// Chiamata all'API delle festività
 $.ajax({
   "url": endpoint,
-  // "data": function(){},
   "method": "GET",
   "success": function(data) {
     var risultati = data.response;
+    // Cicliamo nell'array "risultati"
     for(var i = 0; i < risultati.length; i++) {
-      for(var k in risultati[i]) {
-        console.log(k + ": " + risultati[i][k]);
-      }
+      var dataVacanze = risultati[i].date;
+      console.log(dataVacanze);
+      var vacanza = risultati[i].name;
+      console.log(vacanza);
     }
   },
   "error": function(err) {
