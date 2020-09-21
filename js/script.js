f14b2b7d435035bfea19d4d1a6fb9ae3bc852394
Laura Chelaru <laura.a.chelaru@gmail.com>
2020-09-21 09:42:43 +0200

    completo con select per mese
    manca cambio mese con frecce direzionali

--- js/script.js
+++ js/script.js
@@ -4,64 +4,94 @@ $(document).ready(function(){
 // Settiamo moment in italiano
 moment.locale('it');
 
-// Stabiliamo qual è la data iniziale
-var dataIniziale = moment('2018-12-07');
-var giorno = dataIniziale.format("D");
-var mese = dataIniziale.format("M");
-var anno = dataIniziale.format("YYYY");
-var giorniMese = dataIniziale.daysInMonth();
+// Settiamo il valore di select su "Gennaio"
+$("select#month-select").val("01");
+var valoreSelect = $("select#month-select").val();
 
-// Appendiamo mese e anno in pagina
-$("h1#mese").text(dataIniziale.format("MMMM"));
-$("h2#anno").text(anno);
+// In mase al change del valore della select, visualizziamo il mese scelto nelle options
+$("select#month-select").change(function(){
+  valoreSelect = $(this).val();
+  $("ul#days").empty();
+  stampaMese(valoreSelect);
+});
 
-// Prendiamo il template di handlebars
-var source = $("#day-template").html();
-var template = Handlebars.compile(source);
+stampaMese(valoreSelect);
 
-// Scriviamo il giorno e il mese in base a dataIniziale
-for( var i = giorno; i <= giorniMese; i++) {
-  var contenuto = {
-    "data-template": dataIniziale.format("YYYY-MM-DD"),
-    "day": dataIniziale.format("DD"),
-    "month": dataIniziale.format('MMMM')
-  }
-  // Appendiamo ogni giorno del mese in #days
-  $("#days").append(template(contenuto));
-  // Aggiungiamo 1 giorno alla data iniziale
-  dataIniziale.add(1, "days");
-}
+// FUNZIONE di visualizzazzione mese
+function stampaMese(meseScelto) {
+  // Stabiliamo qual è la data iniziale
+  var dataIniziale = moment('2018-'+meseScelto+'-01');
+  var giorno = dataIniziale.format("D");
+  var mese = dataIniziale.format("M");
+  var anno = dataIniziale.format("YYYY");
+  var giorniMese = dataIniziale.daysInMonth();
 
-// CHIAMATA AJAX
-// Mettiamo l'API in una variabile, settato su gennaio
-var endpoint = "https://flynn.boolean.careers/exercises/api/holidays?year="+anno+"&month="+(mese-1);
+  // Appendiamo mese e anno in pagina
+  $("h1#mese").text(dataIniziale.format("MMMM"));
+  $("h3#anno").text(anno);
 
-// Chiamata all'API delle festività
-$.ajax({
-  "url": endpoint,
-  "method": "GET",
-  "success": function(data) {
-    var risultati = data.response;
-    // Cicliamo nell'array "risultati"
-    for(var i = 0; i < risultati.length; i++) {
-      var dataVacanze = risultati[i].date;  // Scriviamo la la data in una variabile
-      var vacanza = risultati[i].name;      // Scriviamo il nome della festività in una variabile
-      // Guardiamo nell'elenco dei giorni appesi in #days
-      $("#days .single-day").each(function(){
-        // Metiamo l'attributo della data in formato YYYY-MM-DD in una variabile
-        var thisDate = $(this).attr("data-thisdate");
-        // Se dataVacanze coincide con una delle date in #days...
-        if(dataVacanze == thisDate) {
-          // ...scriviamo il nome della festività in .holiday
-          $(this).find(".holiday").text(" - " + vacanza);
-          $(this).addClass("red");
-        }
-      });
+  // Prendiamo il template di handlebars
+  var source = $("#day-template").html();
+  var template = Handlebars.compile(source);
+
+  // Scriviamo il giorno e il mese in base a dataIniziale
+  for( var i = giorno; i <= giorniMese; i++) {
+    var contenuto = {
+      "data-template": dataIniziale.format("YYYY-MM-DD"),
+      "day": dataIniziale.format("DD"),
+      "month": dataIniziale.format('MMMM')
     }
-  },
-  "error": function(err) {
-    alert("Errore!");
+    // Appendiamo ogni giorno del mese in #days
+    $("#days").append(template(contenuto));
+    // Aggiungiamo 1 giorno alla data iniziale
+    dataIniziale.add(1, "days");
+  }
+
+  // CHIAMATA AJAX
+  // Mettiamo l'API in una variabile
+  var endpoint = "https://flynn.boolean.careers/exercises/api/holidays?year="+anno+"&month="+(mese-1);
+
+  // Chiamata all'API delle festività
+  $.ajax({
+    "url": endpoint,
+    "method": "GET",
+    "success": function(data) {
+      var risultati = data.response;
+      // Cicliamo nell'array "risultati"
+      for(var i = 0; i < risultati.length; i++) {
+        var dataVacanze = risultati[i].date;  // Scriviamo la la data in una variabile
+        var vacanza = risultati[i].name;      // Scriviamo il nome della festività in una variabile
+        // Guardiamo nell'elenco dei giorni appesi in #days
+        $("#days .single-day").each(function(){
+          // Metiamo l'attributo della data in formato YYYY-MM-DD in una variabile
+          var thisDate = $(this).attr("data-thisdate");
+          // Se il giorno è una festività, lo coloriamo di rosso e scriviamo la festività
+          if(thisDate == dataVacanze) {
+            $(this).addClass("red");
+            $(this).find(".holiday").text("- " + vacanza);
+          }
+        });
+      }
+    },
+    "error": function(err) {
+      alert("Errore!");
+    }
+  });
+
+$("#days .single-day").each(function(){
+  var thisDate = $(this).attr("data-thisdate");
+  if(moment(thisDate).weekday() == "6" && $(this).hasClass("red")) {
+    $(this).addClass("underline");
+  } else if (moment(thisDate).weekday() == "6" && !$(this).hasClass("red")) {
+    $(this).addClass("blue underline");
   }
 });
+
+
+
+}
+
+
+
 
 });
